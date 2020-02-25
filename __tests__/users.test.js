f7fe1894dc7e5e95ec0a0d5cb607bc6adf543097
Travis Molter <travismolter@gmail.com>
2020-02-24 18:00:50 -0800

    finishing test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -59,45 +59,12 @@ describe('user routes', () => {
     return request(app)
       .get(`/api/v1/users/${user._id}`)
       .then((res) => {
-        expect(res.body).toEqual({
+        expect(res.body).toEqual({ __v: 0,
           _id: expect.any(String),
-          mixtapes: [{
+          mixtapes: [{ __v: 0,
             _id: expect.any(String),
-            userId: user._id,
-            songs: [
-              {
-                _id: expect.any(String),
-                nativeId: '345',
-                nativeSource: 'soundcloud',
-                title: 'Big Poppa',
-                thumbnailUrl: 'mypic.com',
-                buyLink: 'buythis.com',
-                isMemo: false,
-                tags: ['rap']
-              }    
-            ],
-            rating: 1
-          },
-          {
-            _id: expect.any(String),
-            userId: user._id,
-            songs: [
-              {
-                _id: expect.any(String),
-                nativeId: '567',
-                nativeSource: 'youtube',
-                title: 'Lady Gaga',
-                thumbnailUrl: 'mycoolpic.com',
-                buyLink: 'buythissicktrack.com',
-                isMemo: false,
-                tags: ['pop']
-              }    
-            ],
-            rating: 3
-          }],
-          username: 'treesus',
-          __v: 0
-        });
+            rating: 1,
+            songs: [{ '_id': '5e547f4b59cda015c0ee64a5', 'buyLink': 'buythis.com', 'isMemo': false, 'nativeId': '345', 'nativeSource': 'soundcloud', 'tags': ['rap'], 'thumbnailUrl': 'mypic.com', 'title': 'Big Poppa' }], 'userId': '5e547f4959cda015c0ee64a3' }, { '__v': 0, '_id': '5e547f4b59cda015c0ee64a6', 'rating': 3, 'songs': [{ '_id': '5e547f4b59cda015c0ee64a7', 'buyLink': 'buythissicktrack.com', 'isMemo': false, 'nativeId': '567', 'nativeSource': 'youtube', 'tags': ['pop'], 'thumbnailUrl': 'mycoolpic.com', 'title': 'Lady Gaga' }], 'userId': '5e547f4959cda015c0ee64a3' }], 'username': 'treesus' });
       });
       
   });
